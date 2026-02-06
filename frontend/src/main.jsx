bb23e6ab9416ca5f0c123e837cda2301fd2298eb
910862KUMAR <145100981+910862KUMAR@users.noreply.github.com>
2026-02-06 12:06:53 +0530

    added requirements file for backend deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import Recommendations from "./Recommendations";
+import Profile from "./Profile";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+function App() {
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/recommendations" element={<Recommendations />} />
+            <Route path="/profile" element={<Profile />} />
+        </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
