6f5eafa63c4a4930356592549708cc2baed55d02
910862KUMAR <145100981+910862KUMAR@users.noreply.github.com>
2026-02-06 12:31:06 +0530

    moved requirements to root for render

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "./Dashboard";
-import Recommendations from "./Recommendations";
-import Profile from "./Profile";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-function App() {
-  return (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/recommendations" element={<Recommendations />} />
-            <Route path="/profile" element={<Profile />} />
-        </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
